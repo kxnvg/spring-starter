cff4a12f708451552bdd0852e3fcf6564098c78e
kxnvg <nikita.armeec.88@mail.ru>
2023-12-15 22:09:52 +0300

    added basic class, configuration security, db migration, entity, dto

--- spring-starter/src/main/java/org/kxnvg/http/controller/GreetingController.java
+++ spring-starter/src/main/java/org/kxnvg/http/controller/GreetingController.java
@@ -1,6 +1,7 @@
 package org.kxnvg.http.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.kxnvg.dto.UserReadDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
